091f8f32046925177c102a40fef1455a2f64674c
Nova <eldanadesalgne@gmail.com>
2022-01-29 00:00:12 -0500

    completed the assignment part

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -23,7 +23,7 @@ module.exports = () => {
       // Webpack plugin that generates our html file and injects our bundles. 
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Contact Cards'
+        title: 'Jate Cards'
       }),
      
       // Injects our custom service worker
@@ -36,9 +36,9 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'Contact Cards',
-        short_name: 'Contact',
-        description: 'Never forget your contacts!',
+        name: 'Jate Cards',
+        short_name: 'Jate',
+        description: 'Jates text editor!',
         background_color: '#225ca3',
         theme_color: '#225ca3',
         start_url: '/',
